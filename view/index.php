b3d47d557e3045773d30d3eba7802c35b59f1835
Dainerdev <florianarroyo15@gmail.com>
2024-06-24 00:14:35 -0500

    Se agrega la libreria con el orm activerecord, entidades y modificacion del index

---
+++ view/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Bank - Dainer Florian</title>
+</head>
+<body>
+    <div class="container">
+        <div class="title">
+            <h1>Welcome @user to your Bank</h1>
+            <p>Try our new features!</p>
+        </div>
+    </div>
+</body>
+</html>
