7fc396ac114b3ded0b90be2c568053844d08568c
Dainerdev <florianarroyo15@gmail.com>
2024-06-24 23:40:27 -0500

    Alerta para indicarle al usuario un error en las credenciales al inciar sesion

---
+++ view/alerts/credentials.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../components/link_bootstrap.php';?>
+    <title>Alert - WaySy</title>
+</head>
+<body>
+    <div class="alert signin">
+        <div class="container">
+            <h3>CREDENTIALS ERROR!</h3>
+            <p>Credentials do not match.<br>
+                Please, try again.
+            </p>
+            <div class="buttons-alert">
+                <a href="../forms/users/login.php">Ok</a>
+            </div>
+        </div>
+    </div>
+
+    <?php include '../components/script_bootstrap.php';?>
+</body>
+</html>
