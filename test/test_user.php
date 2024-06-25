4ff6d5e8c3f1422ff014194511a2e7ee222026a9
Dainerdev <florianarroyo15@gmail.com>
2024-06-24 20:09:56 -0500

    Prueba de conexion, almacenando en la base de datos

---
+++ test/test_user.php
@@ -0,0 +1,36 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . 'Financial_management/models/entities/User.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . 'Financial_management/models/entities/Goal.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . 'Financial_management/models/entities/Category.php';
+
+
+
+$u = new User();
+$u->id_user = '7502215054';
+$u->type_id = 1;
+$u->name_a = 'Ivan';
+$u->name_b = '';
+$u->last_name = 'Alcantara Martínez';
+$u->email = 'asdadad@gmail.com';
+$u->password = 'asdhaksjd';
+$u->age = '2005-06-10';
+$u->registration_date = '2024-06-24';
+
+try {
+    $u->save();
+    $total = User::count();
+    echo "Usuario guardado correctamente.<br>";
+    echo "Hay un total de $total usuarios almacenados.";
+
+} catch (Exception $error) {
+    $msj = $error->getMessage();
+    if (strstr($msj, "Duplicate") !== false) {
+        echo "El usuario ya existe";
+        $total = User::count();
+        echo "Hay un total de $total usuarios almacenados.";
+
+    }else {
+        echo "Se ha producido un error al intentar guardar el usuario." . $msj;
+    }
+}
+
