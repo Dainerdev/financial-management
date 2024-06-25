c288693ef53fd30a01be86a2bc600e84712c0f20
Dainerdev <florianarroyo15@gmail.com>
2024-06-24 23:11:13 -0500

    Alerta para informar al usuario qeu se ha registrado correctamente

---
+++ view/alerts/registered.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../components/link_bootstrap.php';?>
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="alert signin">
+        <div class="container">
+            <h4>You have successfully registered!</h4>
+            <p>Would you like to Log In?</p>
+            <div class="buttons-alert">
+                <a href="../forms/users/login.php">Ok</a>
+                <a href="../forms/users/signin.php">Back</a>
+            </div>
+        </div>
+    </div>
+
+    <?php include '../components/script_bootstrap.php';?>
+</body>
+</html>
