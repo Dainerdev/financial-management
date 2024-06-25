57313904f7a92ff1d4e87e758a5e3b4c5754d5db
Dainerdev <florianarroyo15@gmail.com>
2024-06-24 20:46:49 -0500

    Version inicial del login

---
+++ view/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'components/link_bootstrap.php';?>
+    <title>Log In - WaySy</title>
+</head>
+<body>
+    <center>
+        <form action="action" method="post">
+            <h2>LOG IN - WaySy</h2>
+            <div class="line"></div>
+            <label for="email">E-Mail:</label>
+            <input type="email" name="email" id="email" required>
+
+            <label for="pass">Password:</label>
+            <input type="password" name="pass" id="pass" required>
+
+            <span>Don't have an account? <a href="../view/signin.php">Sign In now</a></span>
+
+            <button type="submit">Log In</button>
+            <button type="reset">Clean</button>
+        </form>
+    </center>
+
+    <?php include 'components/script_bootstrap.php';?>
+</body>
+</html>
