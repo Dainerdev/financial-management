630c8dc520e086b79cad65977e864644e5ebb9e6
Dainerdev <florianarroyo15@gmail.com>
2024-06-29 01:14:29 -0500

    La vista login es renombrada como index. El antiguo index es renombrado como main

---
+++ view/forms/users/main.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/main.scss">  
+    <title>WaySy</title>
+</head>
+<body>
+    <div class="container">
+        <header class="jumbotron text-center">
+            <h1>Welcome @user to WaySy, your financial manager.</h1>
+            <p>Try our new features!</p>
+        </header>
+    </div>
+
+</body>
+</html>
--- view/index.php
+++ view/index.php
@@ -1,19 +1,52 @@
+<?php
+    $msj = @$_REQUEST["msj"]; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WaySy</title>
-    <?php include 'components/link_bootstrap.php';?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <link href="css/login.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <title>Log In - WaySy</title>
 </head>
 <body>
-    <div class="container">
-        <header class="jumbotron text-center">
-            <h1>Welcome @user to WaySy, your financial manager.</h1>
-            <p>Try our new features!</p>
-        </header>
-    </div>
+    <center>
+        <div class="container-form">        
+            <form action="../controllers/UserController.php" class="form" method="post">
+                <div>
+                    <h1>LOG IN</h2>
+                    <div class="line"></div>
+                    
+                    <div class="inputs">
+                        <label for="email">E-Mail:</label>
+                        <input type="email" name="email" id="email" required placeholder="ex@mple.com">
 
-    <?php include 'components/script_bootstrap.php';?>
+                        <label for="pass">Password:</label>
+                        <input type="password" name="pass" id="pass" required>
+                    </div>
+
+                    <div class="span">
+                        <span>Don't have an account? <a href="../view/forms/users/signin.php">Sign up now</a></span>
+                    </div>
+
+                    <div class="buttons-login">
+                        <input type="submit" name="action" id="action" value="Log In">
+                        <input type="reset" value="Clean" id="clean">
+                    </div> 
+                </div> 
+                <div class="line"></div>          
+                <div class="alert">
+                    <span><?= ($msj != NULL || isset($msj)) ? $msj : "" ?></span>
+                </div>
+                <div class="brand">
+                    <span>
+                        <h2>WaySy</h2>
+                    </span>
+                </div>
+            </form>        
+        </div>
+    </center>
 </body>
 </html>
