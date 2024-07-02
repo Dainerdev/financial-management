2ec5514f2965a0d6c2063f4680aa553e4c3f1330
Dainerdev <florianarroyo15@gmail.com>
2024-07-02 09:48:22 -0500

    Modificación de botones, se agrega la linea decorativa.

--- view/alerts/registered.php
+++ view/alerts/registered.php
@@ -3,19 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/registered.css">
+    <link rel="stylesheet" href="../css/main.css">
     <title>Alert - WaySy</title>
 </head>
-<body>    
-    <div class="alert signin">
-        <div class="container">
-            <h4>YOU HAVE SUCCESSFULLY REGISTERED!</h4>
-            <p>Would you like to Log In?</p>
-            <div class="buttons-alert">
-                <a href="../index.php">Ok</a>
-                <a href="../forms/users/signin.php">Back</a>
+<body>  
+    <center>
+        <div class="alert signin">
+            <div class="container">
+                <h4>YOU HAVE SUCCESSFULLY REGISTERED!</h4>
+                <div class="line"></div>
+                <p>Would you like to Log In?</p>
+                <div class="buttons-alert">
+                    <a href="../index.php" id="ok">Okay</a>
+                    <a href="../forms/users/signin.php" id="back">Back</a>
+                </div>
             </div>
         </div>
-    </div>
-
+    </center>  
 </body>
 </html>
